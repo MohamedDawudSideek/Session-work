b27eb646b37201b428d443c478f9e2abda216160
MohamedDawudSideek <ms.dawud2006@gmail.com>
2024-11-22 05:19:40 -0800

    Second Commit

--- app.js
+++ app.js
@@ -51,38 +51,38 @@
 
 // }
 
-let body = "";
+// let body = "";
 
 
-fetch("https://fakestoreapi.com/products")
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(element => {
-            body += ` 
-<div class="col">
-    <div class="card shadow-sm">
-        <img src="${element.image}" alt="">
-        <div class="card-body">
-            <h5 class="card-text">${element.title}</h5>
-            <p class="card-text">${element.description}</p>
-            <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-body-secondary">${element.price}$</small>
-            </div>
-        </div>
-    </div>
-</div> 
-            `
+// fetch("https://fakestoreapi.com/products")
+//     .then(res => res.json())
+//     .then(data => {
+//         data.forEach(element => {
+//             body += ` 
+// <div class="col">
+//     <div class="card shadow-sm">
+//         <img src="${element.image}" alt="">
+//         <div class="card-body">
+//             <h5 class="card-text">${element.title}</h5>
+//             <p class="card-text">${element.description}</p>
+//             <div class="d-flex justify-content-between align-items-center">
+//                 <div class="btn-group">
+//                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+//                     <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+//                 </div>
+//                 <small class="text-body-secondary">${element.price}$</small>
+//             </div>
+//         </div>
+//     </div>
+// </div> 
+//             `
 
-        });
+//         });
 
-        document.getElementById("ROW").innerHTML = body;
-    })
+//         document.getElementById("ROW").innerHTML = body;
+//     })
 
-    console.log(body);
+//     console.log(body);
 
 
 
