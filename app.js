aa93d9be2f7f6937457b9335bc749d8787e3c612
MohamedDawudSideek <ms.dawud2006@gmail.com>
2024-11-22 05:41:25 -0800

    third Commit

--- app.js
+++ app.js
@@ -53,6 +53,30 @@
 
 // let body = "";
 
+// alert("Hello");
+
+let timerInterval;
+Swal.fire({
+  title: "Auto close alert!",
+  html: "I will close in <b></b> milliseconds.",
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading();
+    const timer = Swal.getPopup().querySelector("b");
+    timerInterval = setInterval(() => {
+      timer.textContent = `${Swal.getTimerLeft()}`;
+    }, 100);
+  },
+  willClose: () => {
+    clearInterval(timerInterval);
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log("I was closed by the timer");
+  }
+});
 
 // fetch("https://fakestoreapi.com/products")
 //     .then(res => res.json())
